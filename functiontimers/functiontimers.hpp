0ad09a464677d071195dd65ad3cad335481da433
doriantgt <41567009+doriantgt@users.noreply.github.com>
2021-09-21 17:20:59 -0600

    Update functiontimers.hpp

--- functiontimers/functiontimers.hpp
+++ functiontimers/functiontimers.hpp
@@ -16,9 +16,20 @@ public:
     uint32_t internalTime32;
     
     bool timePassed16( uint16_t &previousTime, const uint16_t interval ){
-        
-        if(  (uint32_t)(internalTime32 - previousTime) > interval){
+          if(  (uint16_t)internalTime32 - previousTime > interval){
+
+         //   std::cout << "prev  " <<  previousTime << "  internal " << internalTime32  << "/n";
+            previousTime = internalTime32;
+            return true;
             
+        }else{
+            return false;
+        }
+    }
+        bool timePassed32( uint32_t &previousTime, const uint32_t interval ){
+       if( internalTime32 - previousTime > interval){
+      //  if(  (uint32_t)(internalTime32 - previousTime) > interval){
+         //   std::cout << "prev  " <<  previousTime << "  internal " << internalTime32  << "/n";
             previousTime = internalTime32;
             return true;
             
